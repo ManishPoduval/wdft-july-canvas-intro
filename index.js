8f5ed601ca6de29417ec7e6c9e3f6c44dd2bff98
Manish Poduval <manishpoduval@hotmail.com>
2020-07-15 18:05:38 +0530

    gotta catch em all

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let canvas = document.querySelector('canvas')
 canvas.style.border = '2px solid black';
-canvas.style.backgroundColor = 'lightgrey'
+canvas.style.backgroundColor = 'grey'
 
 //canvas.parentNode.removeChild(canvas)
 
@@ -42,39 +42,62 @@ let ctx = canvas.getContext('2d')
 // ctx.stroke()
 
 
+// ctx.beginPath()
+// ctx.arc(200, 200, 100, 0, Math.PI*2 )
+// ctx.stroke()
+// ctx.closePath()
+
+
+// ctx.lineWidth = 10
+ctx.fillStyle = 'white';
+ctx.lineWidth = 10
 ctx.beginPath()
-ctx.arc(200, 200, 100, 0, Math.PI*2 )
+ctx.arc(200, 200, 100, 0, Math.PI)
+ctx.lineTo(300, 200)
+ctx.fill()
 ctx.stroke()
 ctx.closePath()
 
-
+ctx.fillStyle = 'red';
+ctx.lineWidth = 10
 ctx.beginPath()
-ctx.arc(200, 200, 80, 0, Math.PI)
+ctx.arc(200, 200, 100, 0, Math.PI, true)
+
+ctx.fill()
 ctx.stroke()
 ctx.closePath()
 
+ctx.fillStyle = 'white';
+ctx.lineWidth = 20
 ctx.beginPath()
-ctx.arc(160, 160, 15, 0, Math.PI*2 )
+ctx.arc(200, 200, 20, 0, Math.PI*2, true)
 ctx.stroke()
+ctx.fill()
 ctx.closePath()
 
-ctx.beginPath()
-ctx.arc(240, 160, 15, 0, Math.PI*2 )
-ctx.stroke()
-ctx.closePath()
 
-ctx.beginPath()
-ctx.moveTo(200, 200)
-ctx.lineTo(200, 240)
-ctx.stroke()
+// ctx.beginPath()
+// ctx.arc(160, 160, 15, 0, Math.PI*2 )
+// ctx.stroke()
+// ctx.closePath()
 
-ctx.font = '32px Verdana'
-ctx.fillText('Hello Ironhackers', 100, 70)
+// ctx.beginPath()
+// ctx.arc(240, 160, 15, 0, Math.PI*2 )
+// ctx.stroke()
+// ctx.closePath()
 
-let img = new Image()
-img.src = 'https://media.wsimag.com/attachments/8e728306596490ce126e8ac25f1889ca0b6d84ed/store/fill/1230/692/d3ea65079ad3b3a27b4ef63cc646031b58620079d7ccc91e7c8680904587/Monarch-Butterfly.jpg'
+// ctx.beginPath()
+// ctx.moveTo(200, 200)
+// ctx.lineTo(200, 240)
+// ctx.stroke()
 
-img.addEventListener('load', function(){
-    ctx.drawImage(img , 100, 100, 300, 200)
-})
+// ctx.font = '32px Verdana'
+// ctx.fillText('Hello Ironhackers', 100, 70)
+
+// let img = new Image()
+// img.src = 'https://media.wsimag.com/attachments/8e728306596490ce126e8ac25f1889ca0b6d84ed/store/fill/1230/692/d3ea65079ad3b3a27b4ef63cc646031b58620079d7ccc91e7c8680904587/Monarch-Butterfly.jpg'
+
+// img.addEventListener('load', function(){
+//     ctx.drawImage(img , 100, 100, 300, 200)
+// })
 
